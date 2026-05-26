1cb813af45ea7be59979ae53dd6920946638722c
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-27 00:01:39 +0100

    feat: implement tf-molecule-alb-public-aws molecule (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "alb_arn" {
+  description = "ARN of the ALB"
+  value       = module.alb.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = module.alb.dns_name
+}
+
+output "alb_zone_id" {
+  description = "Route53 zone ID of the ALB"
+  value       = module.alb.zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = module.target_group.arn
+}
+
+output "https_listener_arn" {
+  description = "ARN of the HTTPS listener"
+  value       = module.https_listener.arn
 }
